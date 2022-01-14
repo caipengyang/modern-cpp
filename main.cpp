054d6e2812f7baec2d2131decfe6fb67913ef382
yauta <caipengy@mail3.sysu.edu.cn>
2022-01-14 08:05:46 +0000

    init .gitpod

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 using namespace std;
 
+template<typename U, typename V>
+auto add(U a, V b) {
+    return a + b;
+}
+
 int main(){
-    cout<<"hello, world"<<endl;
+    cout<<"hello, world:"<<add(1, 2.3)<<endl;
     return 0;
 }
