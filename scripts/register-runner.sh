b983472f3c813f83612c032f329e091e86c15215
yauta <caipengy@mail3.sysu.edu.cn>
2022-01-15 09:12:51 +0000

    init test.yml

---
+++ scripts/register-runner.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#detect project root path
+PROJECT_ROOT=`git rev-parse --show-toplevel`
+CURRENT_PATH=`pwd`
+
+# Create a folder
+cd $PROJECT_ROOT
+mkdir -p actions-runner && cd actions-runner
+# Download the latest runner package
+curl -o actions-runner-linux-x64-2.286.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.286.0/actions-runner-linux-x64-2.286.0.tar.gz
+# Optional: Validate the hash
+echo "855b84fe96fda84b22bb9efa2088652d785ed856a7af484b5f4c44f650d7eecf  actions-runner-linux-x64-2.286.0.tar.gz" | shasum -a 256 -c
+# Extract the installer
+tar xzf ./actions-runner-linux-x64-2.286.0.tar.gz
+
+# Create the runner and start the configuration experience
+./config.sh --url https://github.com/caipengyang/modern-cpp --token AALY27AHKSLPP3DN6AKOUX3B4KJAW
+# Last step, run it!
+./run.sh
